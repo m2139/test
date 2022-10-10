817c118ab915369f957599a8c3530028b46bdda3
1038456 <1038456@localhost>
2022-10-10 04:03:09 +0000

    can_sousin_KAKUTEI

--- CAN_SEND.cpp
+++ CAN_SEND.cpp
@@ -1,16 +1,22 @@
 #include "CAN_SEND.hpp"
 
-CANSend::CANSend(PinName RD,PinName TD) : can(RD,TD)
-{
-    for(int i = 0;i < 3;i++){
-    DATA[i] = 0;
+CANSend::CANSend(PinName RD,PinName TD) : can(RD,TD){
+    for(int i = 0;i < 8;i++){
+        DATA[i] = 0;
     }
 }
 
-void CANSend::SEND(int id, int data){
-    DATA[0] = data % 100;//下2
-    DATA[1] = data / 100;//上2
-    CANMessage msg(id,DATA,2);
+void CANSend::SEND(int id,int num1,int num2,int num3,int num4)
+{
+    DATA[0] = num1 >> 8;
+    DATA[1] = num1 & 0xff;
+    DATA[2] = num2 >> 8;
+    DATA[3] = num2 & 0xff;
+    DATA[4] = num3 >> 8;
+    DATA[5] = num3 & 0xff;
+    DATA[6] = num4 >> 8;
+    DATA[7] = num4 & 0xff;
+    CANMessage msg(id,DATA,8);
     can.write(msg);
     wait_ms(2);
 }
--- CAN_SEND.hpp
+++ CAN_SEND.hpp
@@ -3,23 +3,39 @@
 #include "mbed.h"
 
 #if 0
+--------------------------------------------------------------------------------
 ~ピン配列~
 F446RE D4 D10
 L432KC D10 D2
 
 ~使用方法~
-CANSend can(RD,TD); 
-can.SEND(1,1234); DATAは四桁以下専用です。五桁以上対応版は気が向いたら作ります.
+•送る側
+CANSend can(RD,TD);
+can.SEND(id,num1,num2,num3,num4);//id,numは自由に指定自由、値は４個まで行けますidは必須numは設定しなくてもいい
+
+•受信側
+CAN can3(RD,TD);
+CANMessage msg1;//CANメッセージ生成
+while(1){
+    can3.read(msg1);
+    if(msg1.id == 1) {
+        num1 = (msg1.data[0] * 16 * 16) + msg1.data[1]; //これらの式で送られてきた値を戻す。
+        num2 = (msg1.data[2] * 16 * 16) + msg1.data[3]; //いじり方次第では値の数を増やせる(値の上限値は小さくなる)
+        num3 = (msg1.data[4] * 16 * 16) + msg1.data[5]; //わからないことがあればM2139までお問い合わせください
+        num4 = (msg1.data[6] * 16 * 16) + msg1.data[7]; //  2022年10月10日   13:00  (月)　制作
+    }
+}
+--------------------------------------------------------------------------------
 #endif
 
 class CANSend
 {
-    
+
 public:
     CANSend(PinName RD,PinName TD);
-    void SEND(int,int);//id data
+    void SEND(int,int num1 = 0,int num2 = 0,int num3 = 0 ,int num4 = 0);//id data
 private:
     CAN can;//can通信宣言
-    char DATA[2];//DATA[0] 下2 DATA[1] 上2
+    char DATA[8];
 };
 #endif
